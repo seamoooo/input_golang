1097f048deef09e64765127efa55501e0b5d52ad
seamoooo <hm.seamo@gmail.com>
2023-01-18 13:55:49 +0900

    render temp

---
+++ inputGoTest/simpleapp/cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+
+	// register routes
+	mux.Get("/", app.Home)
+
+	// static assets
+
+	return mux
+}
