32da2ea2c954e7b84cb985dc5a1c229af15fdc54
seamoooo <hm.seamo@gmail.com>
2023-01-29 23:37:07 +0900

    encode binary

--- SystemPrograming/intro/main.go
+++ SystemPrograming/intro/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 )
 
 func main() {
-	var buffer1 bytes.Buffer
-	bytes.NewBuffer([]byte{0x10})
-	buffer3 := bytes.NewBufferString("初期文字列")
-	fmt.Println(buffer1)
-	fmt.Println(buffer3)
+	data := []byte{0x0, 0x0, 0x27, 0x27}
+
+	var i int32
+	var o int32
+	// byte oderはlitte edianかbig edian かを指定
+	binary.Read(bytes.NewReader(data), binary.LittleEndian, &i)
+	binary.Read(bytes.NewReader(data), binary.BigEndian, &o)
+	fmt.Printf("data: %d\n", i)
+	fmt.Printf("data: %d\n", o)
 }
