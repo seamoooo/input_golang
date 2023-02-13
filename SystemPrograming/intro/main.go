c2f43a096b9425726dfe620956e0c07cffaddb57
seamoooo <hm.seamo@gmail.com>
2023-02-13 10:04:33 +0900

    sys

--- SystemPrograming/intro/main.go
+++ SystemPrograming/intro/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"os"
+	"fmt"
+	"math"
 )
 
+func printNumber() chan int {
+	result := make(chan int)
+
+	go func() {
+		result <- 2
+		for i := 3; i < 100000; i += 2 {
+			l := int(math.Sqrt(float64(i)))
+			fount := false
+			for j := 3; j < l; j += 2 {
+				if i%j == 0 {
+					fount = true
+					break
+				}
+			}
+			if !fount {
+				result <- 1
+			}
+		}
+		close(result)
+	}()
+	return result
+}
+
 func main() {
-	header := bytes.NewBufferString("-------header---------")
-	content := bytes.NewBufferString("-------content---------")
-	footer := bytes.NewBufferString("-------fotter---------")
-
-	reader := io.MultiReader(header, content, footer)
-
-	io.Copy(os.Stdout, reader)
-	// -------header----------------content----------------fotter---------%
+	pn := printNumber()
+	for n := range pn {
+		fmt.Println(n)
+	}
 }
